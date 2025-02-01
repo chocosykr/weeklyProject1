6560c588d6cd16afad3b82d3b63b528da4da9c0a
sykr <sejalraju8@gmail.com>
2025-02-01 15:51:10 +0530

    more stuff

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import SearchForm from "../../components/searchForm";
 
-export default function Home() {
+export default async function Home({ searchParams } : {
+  searchParams: Promise<{ query?: string }>
+
+}) {
+
+  const query = (await searchParams).query;
+
+
   return (
     <>
-    <h1 className="text-work-sans">HOME</h1>
+    <section className="pink_container">
+      <h1 className="heading">Pitch Your Startup <br /> Connect with entrepreneurs</h1>
+
+      <p className="sub-heading !max-w-5xl" >
+        Submit ideas, Vote on Pitches, and get noticed in virtual Competitions
+      </p>
+      
+      <SearchForm query={query} />
+    </section>
     </>
   );
 }
